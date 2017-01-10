ab7c92241d09f4e8b16d23b8113d065263dc781b
Kamus Hadenes <kamushadenes@hyadesinc.com>
2017-01-10 01:02:25 -0200

    Automatic commit message

--- setup.py
+++ setup.py
@@ -8,15 +8,15 @@ if os.path.exists('README.md'):
 
 
 setup(
-  name = '17tracker',
-  packages = ['17tracker'], # this must be the same as the name above
+  name = 'tracker17',
+  packages = ['tracker17'], # this must be the same as the name above
   version = '0.1',
-  description = '17Track.net Python API (Unnoficial)'
+  description = '17Track.net Python API (Unnoficial)',
   long_description = long_description,
   author = 'Kamus Hadenes',
   author_email = 'kamushadenes@hyadesinc.com',
-  url = 'https://github.com/kamushadenes/17tracker', # use the URL to the github repo
-  download_url = 'https://github.com/kamushadenes/17tracker/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/kamushadenes/tracker17', # use the URL to the github repo
+  download_url = 'https://github.com/kamushadenes/tracker17/tarball/0.1', # I'll explain this in a second
   keywords = ['tracking', 'mail', 'api'], # arbitrary keywords
 )
 
